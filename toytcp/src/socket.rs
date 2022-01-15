aeebee43ffa8c1afae7742a9431afaccea387913
s-ichikawa <s-ichikawa@mercari.com>
2022-01-15 20:45:25 +0900

    Up to 3.5.3

--- toytcp/src/socket.rs
+++ toytcp/src/socket.rs
@@ -5,7 +5,7 @@ use pnet::packet::{ip::IpNextHeaderProtocols, Packet};
 use pnet::transport::{self, TransportChannelType, TransportProtocol, TransportSender};
 use pnet::util;
 use std::collections::VecDeque;
-use std::fmt::{self, Display};
+use std::fmt::{self, Display, Formatter};
 use std::net::{IpAddr, Ipv4Addr};
 use std::time::SystemTime;
 use pnet::packet::ip::IpNextHeaderProtocol;
@@ -21,10 +21,56 @@ pub struct Socket {
     pub remote_addr: Ipv4Addr,
     pub local_port: u16,
     pub remote_port: u16,
+    pub send_param: SendParam,
+    pub recv_param: RecvParam,
+    pub status: TcpStatus,
     pub sender: TransportSender,
 }
 
-pub enum TcpStatus {}
+#[derive(Clone, Debug)]
+pub struct SendParam {
+    pub unacked_seq: u32,   // 送信後まだackされてないseqの先頭
+    pub next: u32,          // 次の送信
+    pub window: u16,        // 送信ウィンドウサイズ
+    pub initial_seq: u32,   // 初期送信seq
+}
+
+#[derive(Clone, Debug)]
+pub struct RecvParam {
+    pub next: u32,          // 次受信するseq
+    pub window: u16,        // 受信ウィンドウサイズ
+    pub initial_seq: u32,   // 初期受信seq
+    pub tail: u32,          // 受信seqの最後尾
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub enum TcpStatus {
+    Listen,
+    SynSent,
+    SynRcvd,
+    Established,
+    FinWait1,
+    FinWait2,
+    TimeWait,
+    CloneWait,
+    LastAck,
+}
+
+impl Display for TcpStatus {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            TcpStatus::Listen => write!(f, "LISTEN"),
+            TcpStatus::SynSent=> write!(f, "SYNSENT"),
+            TcpStatus::SynRcvd => write!(f, "SYNRCVD"),
+            TcpStatus::Established => write!(f, "ESTABLISHED"),
+            TcpStatus::FinWait1 => write!(f, "FINWAIT1"),
+            TcpStatus::FinWait2 => write!(f, "FINWAIT2"),
+            TcpStatus::TimeWait => write!(f, "TIMEWAIT"),
+            TcpStatus::CloneWait => write!(f, "CLONEWAIT"),
+            TcpStatus::LastAck => write!(f, "LASTACK"),
+        }
+    }
+}
 
 impl Socket {
     pub fn new(
@@ -32,6 +78,7 @@ impl Socket {
         remote_addr: Ipv4Addr,
         local_port: u16,
         remote_port: u16,
+        status: TcpStatus,
     ) -> Result<Self> {
         let (sender, _) = transport::transport_channel(
             65535,
@@ -42,17 +89,51 @@ impl Socket {
             remote_addr,
             local_port,
             remote_port,
+            send_param: SendParam {
+              unacked_seq: 0,
+                initial_seq: 0,
+                next: 0,
+                window: SOCKET_BUFFER_SIZE as u16,
+            },
+            recv_param: RecvParam {
+                initial_seq: 0,
+                next: 0,
+                window: SOCKET_BUFFER_SIZE as u16,
+                tail: 0,
+            },
+            status,
             sender,
         })
     }
 
-    pub fn send_tcp_packet(&mut self, flag: u8, payload: &[u8]) -> Result<usize> {
+    pub fn send_tcp_packet(
+        &mut self,
+        seq: u32,
+        ack: u32,
+        flag: u8,
+        payload: &[u8],
+    ) -> Result<usize> {
         let mut tcp_packet = TCPPacket::new(payload.len());
         tcp_packet.set_src(self.local_port);
         tcp_packet.set_dest(self.remote_port);
+        tcp_packet.set_seq(seq);
+        tcp_packet.set_ack(ack);
         tcp_packet.set_flag(flag);
-        let sent_size = self.sender.send_to(tcp_packet.clone(), IpAddr::V4(self.remote_addr))
-            .unwrap();
+        tcp_packet.set_window_size(self.recv_param.window);
+        tcp_packet.set_payload(payload);
+        tcp_packet.set_checksum(util::ipv4_checksum(
+            &tcp_packet.packet(),
+            8,
+            &[],
+            &self.local_addr,
+            &self.remote_addr,
+            IpNextHeaderProtocols::Tcp,
+        ));
+        let sent_size = self
+            .sender
+            .send_to(tcp_packet.clone(), IpAddr::V4(self.remote_addr))
+            .context(format!("failed to send: \n{:?}", tcp_packet))?;
+        dbg!("sent", &tcp_packet);
         Ok(sent_size)
     }
 
--- toytcp/src/tcpflags.rs
+++ toytcp/src/tcpflags.rs
@@ -6,3 +6,29 @@ pub const PSH: u8 = 1 << 3;
 pub const RST: u8 = 1 << 2;
 pub const SYN: u8 = 1 << 1;
 pub const FIN: u8 = 1;
+
+pub fn flag_to_string(flag: u8) -> String {
+    let mut flat_str = String::new();
+    if flag & SYN > 0 {
+        flat_str += "SYN ";
+    }
+    if flag & FIN > 0 {
+        flat_str += "FIN ";
+    }
+    if flag & RST > 0 {
+        flat_str += "RST ";
+    }
+    if flag & CWR > 0 {
+        flat_str += "CWR ";
+    }
+    if flag & ECE > 0 {
+        flat_str += "ECE ";
+    }
+    if flag & PSH > 0 {
+        flat_str += "PSH ";
+    }
+    if flag & URG > 0 {
+        flat_str += "URG ";
+    }
+    flat_str
+}
